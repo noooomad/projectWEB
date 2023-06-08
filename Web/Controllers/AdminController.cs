c234d7592deba2bccfc3547f781d74f592cffc21
eduanna <aniuta379@gmail.com>
2023-06-08 12:46:10 +0300

    sdfdffd

---
+++ Web/Controllers/AdminController.cs
@@ -0,0 +1,75 @@
+﻿using BusinessLogic.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public ViewResult Index()
+        {
+            RestaurantEntities db = new RestaurantEntities();
+            return View(db.Users);
+        }
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User us)
+        {
+            if (ModelState.IsValid)
+            {
+                RestaurantEntities db = new RestaurantEntities();
+                db.Users.Add(us);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid data");
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            RestaurantEntities db = new RestaurantEntities();
+            User model = db.Users.Find(id);
+            if (model != null)
+            {
+                List<UserRole> role = db.UserRoles.Where(s => s.User_ID == id).ToList();
+                foreach (var item in role)
+                {
+                    db.UserRoles.Remove(item);
+                }
+                db.Users.Remove(model);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            RestaurantEntities db = new RestaurantEntities();
+            User us = db.Users.Find(id);
+            ViewBag.isEdit = true;
+            return View("Create", us);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User us)
+        {
+            RestaurantEntities db = new RestaurantEntities();
+            db.Entry<User>(us).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Web/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        [Authorize]
+        public ActionResult Index()
+        {
+            RestaurantEntities re = new RestaurantEntities();
+
+            var _user = re.Users.Where(s => s.User_Login == HttpContext.User.Identity.Name).FirstOrDefault();
+
+            List<Item> items = re.CartItems
+                .Where(model => model.Cart.User_ID == _user.User_ID)
+                .Select(model => model.Item)
+                .ToList();
+
+            double sum = 0;
+            foreach (var item in items)
+            {
+                sum += item.Item_Price;
+            }
+            //List<CartItem> cItems = re.CartItems.Where(model => model.Cart.User_ID == _user.User_ID).ToList();
+
+            // calculate total price
+            // pass only items
+
+            CartItemsModel theModel = new CartItemsModel();
+            theModel.cartItems = items;
+            theModel.TotalPrice = Math.Round(sum, 2);
+            
+            return View(theModel);
+        }
+    }
+
+}
