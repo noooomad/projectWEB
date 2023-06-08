894ffd436323ea07fc05ef9efb813845a7d3bba8
galupaiana <iana.galupa@isa.utm.md>
2023-06-08 12:38:45 +0300

    adfsfd

---
+++ Web/Controllers/SignInController.cs
@@ -0,0 +1,83 @@
+﻿using BusinessLogic.DB;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Web.Controllers
+{
+    public class SignInController : Controller
+    {
+        [HttpGet]
+        public ViewResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            RestaurantEntities db = new RestaurantEntities();
+            bool IsValidUser = db.Users.Any(user => user.User_Login.ToLower() ==
+             model.UserLogin && user.User_Password == model.Password);
+            if (IsValidUser)
+            {
+                User user = db.Users.Where(tempik => tempik.User_Login == model.UserLogin).FirstOrDefault();
+                string userData = string.Format("{0}|{1}|{2}", model.UserLogin, model.Password, user.User_Email);
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, model.UserLogin, DateTime.Now, DateTime.Now.AddHours(1), false, userData);
+                FormsAuthentication.SetAuthCookie(model.UserLogin, false);
+                string encTicket = FormsAuthentication.Encrypt(ticket);
+                HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
+                Response.Cookies.Add(cookie);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(UserModel ulog)
+        {
+            RestaurantEntities db = new RestaurantEntities();
+            User tempUser = db.Users.Where(model => model.User_Login == ulog.Login || model.User_Login == ulog.Email).FirstOrDefault();
+            if (tempUser == null)
+            {
+                User user = new User();
+                user.User_Email = ulog.Email;
+                user.User_Login = ulog.Login;
+                user.User_Password = ulog.Password;
+                user.User_Phone=ulog.Phone;
+                user.User_FrstName = ulog.FirstName;
+                user.User_LastName = ulog.LastName;
+                user.User_Adres = ulog.AdressName;
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                UserRole role = new UserRole();
+                role.User_ID = user.User_ID;
+                role.Role_ID = 2;
+                db.UserRoles.Add(role);
+
+                db.SaveChanges();
+                return RedirectToAction("Login", "SignIn", ulog);
+            }
+            return View("login", ulog);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
