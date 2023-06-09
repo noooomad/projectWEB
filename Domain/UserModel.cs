66b35f1804f40fcc95bb91ec850fc6ff9fc54d77
galupaiana <iana.galupa@isa.utm.md>
2023-06-09 19:53:03 +0300

    dfd

---
+++ Domain/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AdressName { get; set; }
+        public virtual ICollection<RoleModel> Roles { get; }
+
+    }
+}
